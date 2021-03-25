20726b615dcd8fdf5544b1533d93bb4bcf0c2135
Erik Nordby <nordby.erik3@gmail.com>
2021-03-25 15:51:28 -0400

    Finished first question

---
+++ 10-hw5-plsql.sql
@@ -0,0 +1,26 @@
+/* Question 2*/
+CREATE OR REPLACE FUNCTION SEARCH_MUTUAL_FUNDS (arg1 varchar(50), arg2 varchar(50))
+    RETURNS TEXT
+    AS $$
+    DECLARE
+    fund_list TEXT := '[';
+    fund_cursor CURSOR FOR SELECT * FROM MUTUAL_FUND;
+    fund_rec MUTUAL_FUND%ROWTYPE;
+    BEGIN
+        OPEN fund_cursor;
+        LOOP
+            FETCH fund_cursor INTO fund_rec;
+            IF NOT FOUND THEN
+                EXIT;
+            END IF;
+
+            IF (fund_rec.description LIKE '%' || arg1 || '%' ) AND (fund_rec.description LIKE '%' || arg2 || '%' ) THEN
+                fund_list := fund_list || ' ' || fund_rec.symbol;
+            end if;
+        END LOOP;
+        fund_list := fund_list || ']';
+        return fund_list;
+    END;
+    $$ LANGUAGE plpgsql;
+
+SELECT SEARCH_MUTUAL_FUNDS('stock', 'bond');
