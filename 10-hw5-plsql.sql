f1d552dce254ff1e2d4cac383013c7a780ccf4a6
Erik Nordby <nordby.erik3@gmail.com>
2021-03-25 17:29:48 -0400

    Added question 4

--- 10-hw5-plsql.sql
+++ 10-hw5-plsql.sql
@@ -24,3 +24,47 @@ CREATE OR REPLACE FUNCTION SEARCH_MUTUAL_FUNDS (arg1 varchar(30), arg2 varchar(3
     $$ LANGUAGE plpgsql;
 
 SELECT SEARCH_MUTUAL_FUNDS('stock', 'bond');
+
+/*Question 3*/
+
+
+/* Question 2*/
+DROP FUNCTION buy_shares(login varchar, symb varchar, num_shares integer);
+CREATE OR REPLACE FUNCTION BUY_SHARES (log varchar(30), symb varchar(30), numb_shares int)
+    RETURNS BOOLEAN
+    AS $$
+    DECLARE
+    shares_val decimal(10, 2);
+    total_shares_val decimal(10, 2);
+    buyer_cap decimal(10, 2);
+    new_buyer_cap decimal(10, 2);
+    last_trx int;
+    BEGIN
+    /*Get the most recent day's value for the stock*/
+    shares_val := (SELECT PRICE FROM closing_price
+            WHERE symbol LIKE symb
+            ORDER BY P_DATE DESC
+            FETCH FIRST ROW ONLY);
+
+    total_shares_val := shares_val*numb_shares;
+
+    buyer_cap := (SELECT AMOUNT FROM TRXLOG
+            WHERE login LIKE log
+            ORDER BY trx_id DESC
+            FETCH FIRST ROW ONLY);
+
+    last_trx := (SELECT MAX(trx_id) FROM trxlog);
+
+    new_buyer_cap := buyer_cap - total_shares_val;
+
+    IF new_buyer_cap < 0 THEN return False;
+    END IF;
+
+    INSERT INTO TRXLOG (trx_id,login,symbol,t_date,action,num_shares,price,amount) VALUES (last_trx+1, log, symb, CURRENT_DATE, 'buy',  numb_shares, shares_val, new_buyer_cap);
+    RETURN TRUE;
+
+    END;
+    $$ LANGUAGE plpgsql;
+
+
+SELECT BUY_SHARES('mike', 'MM', 5)
